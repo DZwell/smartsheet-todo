0801723ff9b0743b9444f06dada886cf6db45e95
Daniel Zwelling <dzwellingmusic@gmail.com>
2018-03-20 06:40:40 -0700

    Install eslint

--- app/smartsheetClient.js
+++ app/smartsheetClient.js
@@ -1,8 +1,9 @@
 const client = require('smartsheet');
-const smartsheet = client.createClient({ accessToken: process.env.SMARTSHEET_TOKEN});
+
+const smartsheet = client.createClient({ accessToken: process.env.SMARTSHEET_TOKEN });
 
 smartsheet.users.listAllUsers().then((users) => {
     console.log(users);
 }).catch((err) => {
     console.log(err);
-})
+});
